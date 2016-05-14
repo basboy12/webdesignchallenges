80fb9ca1423c3d01d442b3a2823a7b3ec4c0cc5c
Kyle Logue <klogue214@gmail.com>
2016-05-14 19:38:57 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// Include Gulp
+var gulp = require('gulp');
+
+// Include Plugins
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect-php');
+var sync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+
+// Error Handling
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+// ========  Tasks  ========
+
+gulp.task('default', ['serve', 'watch']);
+
+gulp.task('serve', function() {
+  connect.server({
+    base: './public'
+  });
+  sync.init({
+    proxy: 'localhost:8000',
+    open: false,
+    notify: false
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('resources/assets/js/**/*.js', ['scripts']);
+  gulp.watch('resources/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('resources/views/**/*.blade.php').on('change', sync.reload);
+});
+
+gulp.task('scripts', function() {
+  return browserify('resources/assets/js/app.js', { debug: true })
+  // .transform(babelify)
+  .bundle()
+  .on('error', handleError)
+  .pipe(source('admin.min.js'))
+  .pipe(gulp.dest('public/js'))
+  .pipe(sync.reload({stream: true}));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('resources/assets/sass/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: 'compact' }))
+  .on('error', handleError)
+  .pipe(autoprefixer('last 2 versions', '> 5%', 'Firefox ESR'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('public/css'))
+  .pipe(sync.reload({stream: true}));
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/app.min.css">
+        <script src="/js/app.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Future Site of Web Design Challenges</h1>
+        </div>
+    </body>
+</html>
